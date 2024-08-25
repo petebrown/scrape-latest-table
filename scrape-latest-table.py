2c98b461719a74a3cd68a2c607c2d95ddf1f83a3
petebrown <15838790+petebrown@users.noreply.github.com>
2024-08-24 21:44:33 -0400

    swapped 11v11 for bbc

--- scrape-latest-table.py
+++ scrape-latest-table.py
@@ -1,9 +1,9 @@
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-from io import StringIO
+import datetime
+from typing import Dict
 
-def construct_url(competition, game_date):
+def construct_11v11_url(competition, game_date):
     game_date = pd.to_datetime(game_date)
     day = game_date.day
     month = game_date.month_name().lower()
@@ -16,9 +16,89 @@ def construct_url(competition, game_date):
     
     return url
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-}
+def date_today():
+    return datetime.datetime.now().strftime('%Y-%m-%d')
+
+def request_json(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/90.0.4430.212 Safari/537.36'
+    }
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+def get_fixtures(start_date, end_date=None):
+    if not end_date:
+        end_date = start_date
+    fixtures = f'https://web-cdn.api.bbci.co.uk/wc-poll-data/container/sport-data-scores-fixtures?selectedEndDate={end_date}&selectedStartDate={start_date}&todayDate={date_today()}&urn=urn%3Abbc%3Asportsdata%3Afootball%3Ateam%3Atranmere-rovers&useSdApi=false'
+    return request_json(fixtures)['eventGroups']
+
+
+def get_resource_id(fixture_info):
+    return fixture_info['secondaryGroups'][0]['events'][0]['tipoTopicId']
+
+def get_match_id(fixture_info):
+    return fixture_info['secondaryGroups'][0]['events'][0]['id']
+
+def get_table(match_id, date = date_today()):
+    table = f'https://web-cdn.api.bbci.co.uk/wc-poll-data/container/football-table?globalContainerPolling=true&matchDate={date}&matchUrn=urn%3Abbc%3Asportsdata%3Afootball%3Aevent%3A{match_id}'
+    return request_json(table)
+
+def get_league_name(data: Dict) -> str:
+    """ 
+        Extract the league name.
+    """
+    return data['tournaments'][0]['name']
+
+def get_league_df(div: Dict) -> pd.DataFrame:
+    """ 
+        Normalize the JSON participants data into a pandas DataFrame. 
+    """
+    return pd.json_normalize(div['participants'])
+
+def process_league_table_df(df: pd.DataFrame, game_date: str, tab_url: str) -> pd.DataFrame:
+
+    df['season'] = '2024/25'
+    
+    df['game_no'] = df[df.name=='Tranmere Rovers'].matchesPlayed.values[0]
+
+    df['game_date'] = pd.to_datetime(game_date)
+
+    df['url'] = tab_url
+
+    df = df[['season', 'game_no', 'game_date', 'rank', 'name', 'matchesPlayed', 'wins', 'draws', 'losses', 'goalsScoredFor', 'goalsScoredAgainst', 'points', 'url']].rename(columns={
+        'rank': 'pos',
+        'name': 'Team',
+        'matchesPlayed': 'Pld',
+        'wins': 'W',
+        'draws': 'D',
+        'losses': 'L',
+        'goalsScoredFor': 'GF',
+        'goalsScoredAgainst': 'GA',
+        'points': 'Pts'
+    })
+
+    return df
+
+def get_league_table(game_date = date_today()):
+    game_date = date_today()
+
+    f = get_fixtures(game_date)
+
+    match_id = get_match_id(f[0])
+
+    t = get_table(match_id)
+
+    league_name = get_league_name(t)
+
+    tab_url = construct_11v11_url(league_name, game_date)
+
+    df = get_league_df(t['tournaments'][0]['stages'][0]['rounds'][0])
+
+    df = process_league_table_df(df, game_date=game_date, tab_url=tab_url)
+
+    return df
 
 tabs_df = pd.read_csv("./data/lge_tables.csv", parse_dates=["game_date"])
 latest_tab = tabs_df.game_date.max()
@@ -33,30 +113,16 @@ if latest_game > latest_tab:
     
     new_games = lge_df[lge_df.game_date > latest_tab].copy()
     
-    new_games['url'] = new_games.apply(lambda x: construct_url(x.competition, x.game_date), axis = 1)
-
-    urls = new_games['url'].to_list()
+    game_dates = new_games['game_date'].to_list()
 
     new_tables = []
 
-    for url in urls:
-        r = requests.get(url, headers = headers)
-        print(r.status_code)
-        doc = BeautifulSoup(r.text, 'lxml')
-        print(doc)
-
-        table = pd.read_html(StringIO(str(doc)))[0]
-
-        table["pos"] = table.index + 1
-        table["url"] = url
-        
-        table = table[['pos', 'Team', 'Pld', 'W', 'D', 'L', 'GF', 'GA', 'Pts', 'url']]
+    for game_date in game_dates:
+        table = get_league_table(game_date)
         
         new_tables.append(table)
     
     new_tables = pd.concat(new_tables)
-    new_tables = new_tables.merge(new_games, on = "url", how = "left")
-    new_tables = new_tables[["season", "game_no", "game_date", "pos", "Team", "Pld", "W", "D", "L", "GF", "GA", "Pts", "url"]]
 
     tabs_df = pd.concat([tabs_df, new_tables]).sort_values(["game_date", "pos"], ascending = [0, 1]).reset_index(drop = True)
 
